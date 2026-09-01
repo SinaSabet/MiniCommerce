282d583015972238e8a64d981483ba21d4656e36
SinaSabetghadam <sabetghadamsina04@gmail.com>
2026-09-02 01:16:04 +0330

    fix

--- Inventory.Application/EventHandlers/InventoryReservedDomainEventHandler.cs
+++ Inventory.Application/EventHandlers/InventoryReservedDomainEventHandler.cs
@@ -2,31 +2,34 @@
 using Inventory.Domain.DomainEvents;
 using MassTransit;
 using MediatR;
-
-public class InventoryReservedDomainEventHandler
-    : INotificationHandler<InventoryReservedDomainEvent>
+namespace Inventory.Application.EventHandlers
 {
-    private readonly IPublishEndpoint _publishEndpoint;
+
+    //public class InventoryReservedDomainEventHandler
+    //    : INotificationHandler<InventoryReservedDomainEvent>
+    //{
+    //    private readonly IPublishEndpoint _publishEndpoint;
 
 
-    public InventoryReservedDomainEventHandler(
-        IPublishEndpoint publishEndpoint)
-    {
-        _publishEndpoint = publishEndpoint;
-    }
+    //    public InventoryReservedDomainEventHandler(
+    //        IPublishEndpoint publishEndpoint)
+    //    {
+    //        _publishEndpoint = publishEndpoint;
+    //    }
 
 
-    public async Task Handle(
-        InventoryReservedDomainEvent notification,
-        CancellationToken cancellationToken)
-    {
+    //    public async Task Handle(
+    //        InventoryReservedDomainEvent notification,
+    //        CancellationToken cancellationToken)
+    //    {
 
-        await _publishEndpoint.Publish(
-            new InventoryReservedIntegrationEvent(
-                notification.OrderId,
-                notification.ReservationId,
-                []
-            ),
-            cancellationToken);
-    }
+    //        await _publishEndpoint.Publish(
+    //            new InventoryReservedIntegrationEvent(
+    //                notification.OrderId,
+    //                notification.ReservationId,
+    //                []
+    //            ),
+    //            cancellationToken);
+    //    }
+    //}
 }
